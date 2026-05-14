34ccd0a0a2c32ea48a4a7847323eeb884f6a1ab7
Luigi Podda <72930159+lupodda@users.noreply.github.com>
2026-05-14 15:29:40 +0200

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.py
@@ -0,0 +1,40 @@
+from functools import cache
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+
+        #            aab
+        #      /           \
+        #   a,ab           aab
+        #   /   \         /    \
+        #a,a,b  a,ab   aa,b     aab
+
+        res = []
+        @cache
+        def isPalindrome(left, right):
+            while left <right:
+                if s[left] == s[right]:
+                    left+=1
+                    right-=1
+                else:
+                    return False
+
+            return True
+
+        def backtrack(subset, index):
+            # if split is at the end of s we append subset to res
+
+            if index == len(s):
+                res.append(subset.copy())
+                return
+
+            for end in range(index, len(s)):
+                if isPalindrome(index, end):
+                    subset.append(s[index:end+1])
+                    backtrack(subset, end+1)
+                    subset.pop()
+
+        backtrack([], 0)
+        return res
+
+        # INDEXES!!!
+
