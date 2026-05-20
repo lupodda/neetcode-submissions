878b28828e286b98ec3eb7a51b1dc711dea1306f
Luigi Podda <72930159+lupodda@users.noreply.github.com>
2026-05-20 22:16:10 +0200

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.py
@@ -0,0 +1,40 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dirs = [(-1,0),(0,1),(1,0),(0,-1)]
+        queue = deque()
+        time = 0
+        fresh = 0
+
+        def isWithinBounds(r,c):
+            return 0 <= r < m and 0 <= c < n
+
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 1:
+                    fresh +=1
+                    
+                if grid[r][c] == 2:
+                    queue.append((r,c))
+
+        while queue and fresh > 0:
+            length = len(queue)
+
+            for i in range(length):
+                r,c = queue.popleft()
+                for dr, dc in dirs:
+                    new_r, new_c = r+dr, c+dc
+                    if isWithinBounds(new_r, new_c) and grid[new_r][new_c] == 1:
+                        grid[new_r][new_c] = 2
+                        queue.append((new_r,new_c))
+                        fresh -= 1
+            time += 1
+
+        return time if fresh == 0 else -1
+                    
+
+
+
+        
