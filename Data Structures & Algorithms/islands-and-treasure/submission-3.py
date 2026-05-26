11a91435ae6a975d31482093f3863325b85363e6
Luigi Podda <72930159+lupodda@users.noreply.github.com>
2026-05-26 08:47:13 +0200

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.py
@@ -0,0 +1,28 @@
+from collections import deque
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        m=len(grid)
+        n=len(grid[0])
+
+        dirs=[(-1,0),(0,1),(1,0),(0,-1)]
+        land=2147483647
+        queue=deque()
+
+        def isWithinBounds(r,c):
+            return 0<=r<m and 0<=c<n
+    
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c]==0:
+                    queue.append((r,c))
+
+        while queue:
+            r,c=queue.popleft()
+
+            for dr,dc in dirs:
+                new_r,new_c=r+dr,c+dc
+                if isWithinBounds(new_r,new_c) and grid[new_r][new_c]==land:
+                    grid[new_r][new_c] = grid[r][c]+1
+                    queue.append((new_r,new_c))
+
+            
