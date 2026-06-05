1d42f80dd44a06f5bb804cbe869f0d87d95f0d8f
Luigi Podda <72930159+lupodda@users.noreply.github.com>
2026-06-05 09:34:34 +0200

    Add: surrounded-regions - submission-10

---
+++ Data Structures & Algorithms/surrounded-regions/submission-10.py
@@ -0,0 +1,43 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        m = len(board)
+        n = len(board[0])
+
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+        queue = deque()
+
+        def isWithinBounds(r, c):
+            return 0 <= r < m and 0 <= c < n
+
+        for r in range(m):
+            if board[r][0] == "O":
+                board[r][0]="S"
+                queue.append((r, 0))
+            if board[r][n - 1] == "O":
+                board[r][n-1]="S"
+                queue.append((r, n - 1))
+
+        for c in range(n):
+            if board[0][c] == "O":
+                board[0][c]="S"
+                queue.append((0, c))
+            if board[m - 1][c] == "O":
+                board[m-1][c]="S"
+                queue.append((m - 1, c))
+
+        while queue:
+            r, c = queue.popleft()
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if isWithinBounds(nr, nc) and board[nr][nc] == "O":
+                    board[nr][nc] = "S"
+                    queue.append((nr,nc))
+
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == "S":
+                    board[r][c] = "O"
+                elif board[r][c] == "O":
+                    board[r][c] = "X"
