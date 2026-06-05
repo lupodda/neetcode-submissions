879c65fc896551a6c2284d84ebfa5c705204d648
Luigi Podda <72930159+lupodda@users.noreply.github.com>
2026-06-05 09:37:36 +0200

    Add: surrounded-regions - submission-12

---
+++ Data Structures & Algorithms/surrounded-regions/submission-12.py
@@ -0,0 +1,39 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        m=len(board)
+        n=len( board[0])
+
+        directions=[(-1,0),(0,1),(1,0),(0,-1)]
+
+        queue=deque()
+        def isWithinBounds(r,c):
+            return 0<=r<m and 0<=c<n
+
+
+        # remember to split the conditions because then you don't know which index
+        for r in range(m):
+            if board[r][0] =="O": 
+                queue.append((r,0))
+            if board[r][n-1] =="O":
+                queue.append((r,n-1))
+        for c in range(n):
+            if board[0][c]=="O" :
+                queue.append((0,c))
+            if board[m-1][c]=="O":
+                queue.append((m-1,c))
+
+        while queue:
+            r,c =queue.popleft()
+            board[r][c] = "S"
+            for dr, dc in directions:
+                nr, nc=r+dr,c+dc
+                if isWithinBounds(nr,nc) and board[nr][nc]=="O":
+                    board[nr][nc]="S"
+                    queue.append((nr,nc)) #FORGOT 
+
+        for r in range(m):
+            for c in range(n):
+                if board[r][c]=="S":
+                    board[r][c]="O"
+                elif board[r][c]=="O":
+                    board[r][c]="X"
